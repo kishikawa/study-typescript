a023b515af905d11415b22f906951be38693014b
kishikawa <kzishikawa07@gmail.com>
2025-12-30 22:13:34 +0900

    commit

--- frontend-react/src/Greeting.tsx
+++ frontend-react/src/Greeting.tsx
@@ -1,12 +1,14 @@
 // src/Greeting.tsx
 type Props = {
   name: string;
+  age?: string;
 };
 
-const Greeting = ({ name }: Props) => {
+const Greeting = ({ name, age }: Props) => {
   return (
     <h2 style={{ color: '#646cff' }}>
       Hello, {name || 'World'}!
+      {age && <span> (Age: {age})</span>} {/* age が存在する場合のみ表示 */} 
     </h2>
   );
 };
